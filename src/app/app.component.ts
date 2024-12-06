d776fa186ab770b05468a95fcb63bcad0d1e7856
Im-Erik <erik.orrego.n@gmail.com>
2024-12-05 22:33:44 -0300

    pruebas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodoComponent } from './pages/todo/todo.component';
+import { TODO_DATA } from './assets/todo';
+import { NTodo } from './models/todo.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodoComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'ensayoAngular';
+  todoData= TODO_DATA;
+
+
+  getTodoInfo(val: NTodo.TodoData) {
+    console.log(val);
+  }
 }
